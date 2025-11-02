b16e992783396290c4193b2625f017fb3551c332
ngalang290898-svg <ngalang290898@gmail.com>
2025-11-02 20:58:48 +0800

    Update page.tsx

--- app/one-stop/page.tsx
+++ app/one-stop/page.tsx
@@ -1,1 +1,14 @@
+// app/one-stop/page.tsx
+export default function OneStopPage() {
+  return (
+    <div className="container mx-auto py-10">
+      <h1 className="text-3xl font-heading mb-6">One-Stop Centre</h1>
+      <ul className="list-disc pl-5 space-y-2">
+        <li>Curriculum: [links to syllabus, textbooks, etc.]</li>
+        <li>Student Affairs: [policies, forms, etc.]</li>
+        <li>Co-curricular: [clubs, sports schedules, etc.]</li>
+      </ul>
+    </div>
+  );
+}
 
